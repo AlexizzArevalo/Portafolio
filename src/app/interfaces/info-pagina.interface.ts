e597d2a7af060b8ce38c106baa2595f3f2fdaef0
AlexizzArevalo <dalexis.da@gmail.com>
2019-06-03 22:47:27 -0600

    animaciones

---
+++ src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,10 @@
+export interface InfoPagina{
+  titulo?: String,
+  email?: String,
+  nombre_corto?: String,
+  facebook?: String,
+  twitter?: String,
+  instagram?: String,
+  equipo_trabajo?: any []
+}
+
